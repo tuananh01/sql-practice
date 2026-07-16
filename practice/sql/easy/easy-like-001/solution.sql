81b28a5b66c8080d3410b2a278352629730c5f73
tuananh01 <86756286+tuananh01@users.noreply.github.com>
2026-07-16 04:33:06 +0000

    solve(sql/easy): easy-like-001

---
+++ practice/sql/easy/easy-like-001/solution.sql
@@ -0,0 +1,11 @@
+-- Xom Data · Basic-plan subscribers
+-- Problem: https://xomdata.com/practice/easy-like-001
+-- Solved: 2026-07-16
+
+SELECT
+    phone_number,
+    full_name,
+    plans
+FROM subscribers
+WHERE plans LIKE '%basic%'
+ORDER BY full_name
