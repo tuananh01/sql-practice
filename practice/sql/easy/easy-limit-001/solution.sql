7f085be20135f48b7faf6557d027ca5f814cac6f
tuananh01 <86756286+tuananh01@users.noreply.github.com>
2026-07-19 05:52:35 +0000

    solve(sql/easy): easy-limit-001

---
+++ practice/sql/easy/easy-limit-001/solution.sql
@@ -0,0 +1,10 @@
+-- Xom Data · 5 cheapest products
+-- Problem: https://xomdata.com/practice/easy-limit-001
+-- Solved: 2026-07-19
+
+SELECT 
+    name,
+    price 
+FROM products 
+ORDER BY price
+LIMIT 5;
