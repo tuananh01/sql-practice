e45f659ab207bf464c0075aa7aa386ca5fc8e504
tuananh01 <86756286+tuananh01@users.noreply.github.com>
2026-07-23 16:16:38 +0000

    solve: 2 problems (2026-07-23)
    - easy-distinct-006
    - easy-count-006

---
+++ practice/sql/easy/easy-distinct-006/solution.sql
@@ -0,0 +1,8 @@
+-- Xom Data · Types of bank accounts
+-- Problem: https://xomdata.com/practice/easy-distinct-006
+-- Solved: 2026-07-23
+
+SELECT DISTINCT
+    account_type
+FROM accounts 
+ORDER BY account_type
